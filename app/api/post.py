968c083b0035804016f176678e3c4745e50c2764
hubaek <zjfl3122@naver.com>
2025-06-02 19:59:37 +0900

    feat : Post api router 추가

---
+++ app/api/post.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.db.database import get_db
+from app.schemas.post import PostCreate, PostUpdate, PostOut
+from app.crud.post import *
+
+router = APIRouter()
+
+@router.get("/", response_model=list[PostOut])
+def read_posts(db: Session = Depends(get_db)):
+    return get_posts(db)
+
+@router.get("/posts/{post_id}", response_model=PostOut)
+def read_post(post_id: int, db: Session = Depends(get_db)):
+    db_post = get_post(db, post_id)
+    if not db_post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return db_post
+
+@router.post("/posts/", response_model=PostOut)
+def create_post_route(post: PostCreate, db: Session = Depends(get_db)):
+    return create_post(db, post)
+
+@router.put("/posts/{post_id}", response_model=PostOut)
+def update_post(post_id: int, post: PostUpdate, db: Session = Depends(get_db)):
+    return update_post(db, post_id, post)
+
+@router.delete("/posts/{post_id}")
+def delete_post(post_id: int, db: Session = Depends(get_db)):
+    return delete_post(db, post_id)
